d2dd18009ede4b6023023e2689ff6daa223ec0e2
Renata Mie <renata_mie@hotmail.com>
2021-03-23 14:39:13 -0300

    update files

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ require("dotenv").config();
 const express = require ("express");
 const https = require ("https");
 const bodyParser = require ("body-parser");
+const ejs= require ("ejs");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+
 
 app.get("/", function(req, res){
-	res.sendFile(__dirname + "/index.html");
+	res.render("home");
 });
 
 app.post("/", function(req, res){
@@ -23,11 +28,16 @@ app.post("/", function(req, res){
 			const temp = weatherData.main.temp;
 			const description = weatherData.weather[0].description;
 			const icon = weatherData.weather[0].icon;
-			const imageurl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+			const imageurl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+			
+			
 			res.write("<h1>The temperature in "+ query+ " is " + temp + " degrees Celsius. </h1>");
 			res.write("<p>The weather is currently " + description + "<p>");
 			res.write("<img src="+imageurl+">");
 			res.send();
+
+			
 		})
 	})
 })
